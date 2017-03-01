c08f9c66919396d1187809773d5d09946cb1971a
lwynwai <wynwai11@gmail.com>
2017-03-02 00:03:07 +0800

    added validations and shorten method

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,18 @@
 class Url < ApplicationRecord
+    require 'uri'
+
+    class Url < ActiveRecord::Base
+    # This is Sinatra! Remember to create a migration!
+        before_create :shorten, :counter
+        validates :long_url, :presence => true, :format => URI::regexp(%w(http https))
+
+        def shorten
+            range = [*'0'..'9',*'A'..'Z',*'a'..'z']
+
+            @short_url = (0...6).map{ range.sample }.join
+            self.short_url = @short_url
+        end
+        
+    end
+
 end
